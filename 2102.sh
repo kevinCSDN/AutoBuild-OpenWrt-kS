80e3c68514be52b22c7349da5d93f3ba602f78ef
Link-op <40344950+Link-op@users.noreply.github.com>
2022-03-01 20:02:15 +0800

    更新 2102.sh

--- 2102.sh
+++ 2102.sh
@@ -13,3 +13,4 @@ sed -i 's/192.168.1.1/192.168.0.252/g' openwrt/package/base-files/files/bin/conf
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/default-settings/files/zzz-default-settings
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd    # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效（去掉uci前面的
