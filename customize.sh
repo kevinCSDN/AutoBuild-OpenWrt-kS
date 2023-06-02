e89e161c34617a69ff00d1272f9c48bd5c96d3f3
link-op <40344950+14link@users.noreply.github.com>
2023-06-02 19:53:25 +0800

    1

--- customize.sh
+++ customize.sh
@@ -14,7 +14,7 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kiSir '/g" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-ki'/g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" openwrt/package/base-files/files/bin/config_generate
 #
 #
 #
