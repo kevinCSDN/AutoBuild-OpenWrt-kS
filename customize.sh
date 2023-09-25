99eaa032e92ca76c1623bed1b028ad12424965f8
link-op <40344950+14link@users.noreply.github.com>
2023-09-25 20:04:52 +0800

    更新 customize.sh

--- customize.sh
+++ customize.sh
@@ -13,7 +13,7 @@
 		sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 #3. Replace with JerryKuKu’s Argon
 	#	 rm openwrt/package/lean/luci-theme-argon -rf
-		sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kiSir '/g" openwrt/package/lean/default-settings/files/zzz-default-settings
+		sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" openwrt/package/lean/default-settings/files/zzz-default-settings
 		sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" openwrt/package/base-files/files/bin/config_generate
 		git clone https://github.com/sirpdboy/luci-app-netdata openwrt/package/luci-app-netdata
 #
