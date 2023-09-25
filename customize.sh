6f0249e1df3af88e45abd77cda95f0dd309767e6
link-op <40344950+14link@users.noreply.github.com>
2023-09-25 20:07:30 +0800

    更新 customize.sh

--- customize.sh
+++ customize.sh
@@ -15,7 +15,6 @@
 	#	 rm openwrt/package/lean/luci-theme-argon -rf
 		sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" openwrt/package/lean/default-settings/files/zzz-default-settings
 		sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" openwrt/package/base-files/files/bin/config_generate
-		git clone https://github.com/sirpdboy/luci-app-netdata openwrt/package/luci-app-netdata
 #
 #
 #
